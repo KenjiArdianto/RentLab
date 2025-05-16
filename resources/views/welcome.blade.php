674d2f443e3e7432f06dc5bc81f2946cb7536a83
Devi Paramita <dev28paramita@gmail.com>
2025-05-16 13:39:05 +0700

    CobaCommit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel RENTLAB PUNYA EHEHEHE COBA PUSH NIH BOS</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
