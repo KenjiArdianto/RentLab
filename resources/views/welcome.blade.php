5afb476d0f7c665b250c02c3056315863876d241
Devi Paramita <dev28paramita@gmail.com>
2025-05-16 13:50:32 +0700

    halo ini baru lagi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Belajar Github tes </title>
+        <title>Belajar Github tes halo oll</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
