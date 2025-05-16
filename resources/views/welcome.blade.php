03250fc1a015c4aab76c6a92c8224e4f88f5968f
DrSonnnXJ <elsonrajinbelajar@gmail.com>
2025-05-16 13:37:32 +0700

    hi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel kenji</title>
+        <title>Laravel kenji anjing</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
